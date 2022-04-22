0573361b348f6acb7b46665dc29d538b8f9e7043
Chris <bluman325@gmail.com>
2022-04-22 13:51:32 -0500

    fixing bugs in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import 
-
+import CreatePost from './Components/CreatePost';
 
 function App() {
-  const createPost =(newPost) 
-   
+
+  const [posts, setPosts] = useState
+
+  function addNewPost(post){
+    let tempPosts = [post, ...posts]
+    setPosts(tempPosts)
+  }
+
+  return (
+    <div className="App">
+      <CreatePost addNewPost={addNewPost}/>
+      
+    </div>
+  );
 }
 
 export default App;
